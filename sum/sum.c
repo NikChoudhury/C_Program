084f34f6f5d4f426611349fbc27538120bb7dd5f
Nikumani Choudhury <nikunchoudhury@gmail.com>
2020-12-14 18:08:25 +0530

    C program

---
+++ sum/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Please Enter First number : ");
+    scanf("%d", &a);
+    printf("Please Enter Sceond number : ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("Sum = %d", sum);
+    getch();
+    return 0;
+}
